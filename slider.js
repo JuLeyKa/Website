e04ef62ce35887a460ccddb2f9f685dc23120e9b
Halil Zabun <zabzarabun@gmail.com>
2025-01-25 00:51:39 +0100

    Kontaktformular aktualisiert

---
+++ slider.js
@@ -0,0 +1,56 @@
+const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slider .slide");
+const prevButton = document.querySelector(".slider-prev");
+const nextButton = document.querySelector(".slider-next");
+
+let currentSlide = 0;
+let autoSlideInterval; // Timer für automatischen Slide
+
+// Geschwindigkeit in Millisekunden (z.B. 3000 = 3 Sekunden)
+const SLIDE_SPEED = 3000;
+
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${(index - currentSlide) * 100}%)`;
+  });
+}
+
+function showNextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  updateSlider();
+}
+
+function showPrevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  updateSlider();
+}
+
+// Automatische Bewegung starten
+function startAutoSlide() {
+  autoSlideInterval = setInterval(showNextSlide, SLIDE_SPEED);
+}
+
+// Automatische Bewegung stoppen
+function stopAutoSlide() {
+  clearInterval(autoSlideInterval);
+}
+
+// Buttons für manuelles Navigieren
+if (nextButton) {
+  nextButton.addEventListener("click", () => {
+    showNextSlide();
+    stopAutoSlide();
+    startAutoSlide(); // Timer neu starten
+  });
+}
+
+if (prevButton) {
+  prevButton.addEventListener("click", () => {
+    showPrevSlide();
+    stopAutoSlide();
+    startAutoSlide(); // Timer neu starten
+  });
+}
+
+updateSlider();
+startAutoSlide();
